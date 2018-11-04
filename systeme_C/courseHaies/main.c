f857e3ee14cadeb61a2b16d0960b958fb2e95b1e
bastien <bastien.vincent41@yahoo.com>
2018-11-04 23:44:32 +0100

    finition course de haies

--- systeme_C/courseHaies/main.c
+++ systeme_C/courseHaies/main.c
@@ -3,76 +3,63 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h>
 
-void simuler_equipe(int x)
+void simuler_equipe(int x, int tabTemp[24], int tabChute[24], char *equipe)
 {
+    x = x+1;
     int temp;
     int chute;
     int somme = 0;
-    pid_t pidTmp = getpid();
-    printf("L'équipe %d au départ.\n", pidTmp);
+    printf("L'équipe %s au départ.\n", equipe);
     sleep(2);
     for (int i = 0; i < 4; i++)
     {
-        printf("-> le coureur %d de l'équipe %d est parti.\n", (i+1), pidTmp);
-        temp = nbRandom(8,12,(10*x+i));
-        chute = nbRandom(0,10,(100*x+i));
-        if(chute == 1){
+        printf("-> le coureur %d de l'équipe %s est parti.\n", (i + 1), equipe);
+        srand(tabTemp[(x * i)]);
+        temp = 8 + rand() % (11 - 8 + 1);
+        srand(tabChute[(x * i)]);
+        chute = rand() % (10 + 1);
+        if (chute == 1)
+        {
+            printf("-- Chute équipe %s --\n",equipe);
             exit(0);
         }
         sleep(temp);
         somme = somme + temp;
     }
-    printf("L'équipe %d est arrivée en %ds !\n", pidTmp, somme);
+    printf("L'équipe %s est arrivée en %ds !\n", equipe, somme);
     exit(0);
 }
 
-int nbRandom(int min, int max, int seed){
-    srand(seed);
-    int nb = min + rand() % (max - min + 1);
-    return nb;
-}
-
-int main()
+int main(int argc, char** argv)
 {
+    printf("* initialisation \n");
+    srand(time(NULL));
+    int tabTemp[24];
+    int tabChute[24];
+    pid_t tabPid[argc];
+    for (int i = 0; i < 24; i++)
+    {
+        tabTemp[i] = rand();
+        tabChute[i] = rand();
+    }
+    sleep(1);
     printf("* Début Course\n");
-    pid_t pid = fork();
-    if (pid == 0)
+    for (int i = 1; i < argc; i++)
     {
-        simuler_equipe(1);
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            simuler_equipe(i, tabTemp, tabChute, argv[i]);
+        }
+        tabPid[i-1]=pid;
     }
-    pid_t pid1 = fork();
-    if (pid1 == 0)
+    for (int i = 0; i < argc; i++)
     {
-        simuler_equipe(2);
+        int wstatus;
+        waitpid(tabPid[i], &wstatus, 0);
     }
-    pid_t pid2 = fork();
-    if (pid2 == 0)
-    {
-        simuler_equipe(3);
-    }
-    pid_t pid3 = fork();
-    if (pid3 == 0)
-    {
-        simuler_equipe(4);
-    }
-    pid_t pid4 = fork();
-    if (pid4 == 0)
-    {
-        simuler_equipe(5);
-    }
-    pid_t pid5 = fork();
-    if (pid5 == 0)
-    {
-        simuler_equipe(6);
-    }
-    int wstatus;
-    waitpid(pid, &wstatus, 0);
-    waitpid(pid1, &wstatus, 0);
-    waitpid(pid2, &wstatus, 0);
-    waitpid(pid3, &wstatus, 0);
-    waitpid(pid4, &wstatus, 0);
-    waitpid(pid5, &wstatus, 0);
     printf("* Fin Course\n");
     return 0;
 }
